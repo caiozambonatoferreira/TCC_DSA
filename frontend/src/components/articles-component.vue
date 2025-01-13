dda88bae0b9d3bcb8b1fe7a29eb15b38f6714aac
Caio Ferreira <caiozambonato@outlook.com>
2025-01-13 00:18:32 -0300

    feat: showing cards

--- frontend/src/components/articles-component.vue
+++ frontend/src/components/articles-component.vue
@@ -3,7 +3,24 @@
         <p v-if="loading">Carregando...</p>
 
         <div v-else>
-            {{ articles }}
+            <div class="cards" v-if="articles.length">
+                <div class="card" v-for="(article, index) in articles" :key="index">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img :src="article.image" :alt="article.title"/>
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            {{ article.title }}
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a :href="article.url" target="_blank" class="card-footer-item">Ver artigo</a>
+                    </footer>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
@@ -37,3 +54,14 @@ export default {
     }
 }
 </script>
+
+
+<style>
+    .cards {
+        max-width: 1280px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 3rem;
+    }
+</style>
